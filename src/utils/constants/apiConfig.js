735eca07d73f792cb1c322146b1f40844e9f57d0
vkletsko <v.kletsko@gmail.com>
2024-10-26 14:51:51 +0300

    fix: campers api full url

--- src/utils/constants/apiConfig.js
+++ src/utils/constants/apiConfig.js
@@ -1,4 +1,2 @@
-const apiKey = import.meta.env.VITE_MOCK_API_KEY;
-
-export const BASE_URL = `https://${apiKey}.mockapi.io`;
+export const BASE_URL = `https://66b1f8e71ca8ad33d4f5f63e.mockapi.io/campers`;
 export const PER_PAGE = 4;
